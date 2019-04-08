2912142a7aba10e229fbab5014c0a30ddb75a326
Jesus Mora <jesusmoradaw@gmail.com>
2019-04-08 12:24:30 +0200

    Primeros cambios

--- Ejercicios/src/es/studium/Ejercicio1/Programa.java
+++ Ejercicios/src/es/studium/Ejercicio1/Programa.java
@@ -1,16 +1,27 @@
 package es.studium.Ejercicio1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Programa {
         
     	public static void main(String[] args) throws Exception {
     		String numero = "";
-    		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-    		System.out.println("Escribe un número entero");
-    		numero = entrada.readLine();
-    		System.out.println("El cuadrado del número " + numero + " es: " + (Integer.parseInt(numero) * Integer.parseInt(numero)));
+    		numero = pedirNumero();
+    		System.out.println("El cuadrado del número " + numero + " es: " + calcularCuadrado(numero));
     	}
 
+		private static int calcularCuadrado(String numero) {
+			return Integer.parseInt(numero) * Integer.parseInt(numero);
+		}
+
+		private static String pedirNumero() throws IOException {
+			String numero;
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    		System.out.println("Escribe un número entero");
+    		numero = entrada.readLine();
+			return numero;
+		}
+    
 }
